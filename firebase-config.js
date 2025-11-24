6970c4a40ff8fc0de69be362ea96e018e5091df4
roni2026 <hridoyjsb@gmail.com>
2025-11-24 23:17:46 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBAYXdb5CrlNOvI4VMEgg5XkdkDl7S6S-4",
+  authDomain: "bikroytracker.firebaseapp.com",
+  databaseURL: "https://bikroytracker-default-rtdb.firebaseio.com",
+  projectId: "bikroytracker",
+  storageBucket: "bikroytracker.firebasestorage.app",
+  messagingSenderId: "58720742362",
+  appId: "1:58720742362:web:a99c413e28b8a1f61eeee3"
+};
+
+export default firebaseConfig;
